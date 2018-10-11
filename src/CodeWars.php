cf708b3f11a95bf327f9672931410d0179ef2a49
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-10-10 20:34:30 -0400

    Core code

---
+++ src/CodeWars.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PHPProjectGen.
+ * User: edeoleo@gmail.com
+ * Date: 10/11/2018
+ * Time: 12:29 AM
+ */
+
+namespace Elminson\CodeWars;
+
+/**
+ *
+ *
+ */
+class CodeWars
+{
+    /**
+     * PHPProjectGen constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        return "index";
+    }
+
+
+
+}
---
+++ tests/testCodeWars.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elminsondeoleobaez
+ * Date: 10/3/18
+ * Time: 1:52 PM
+ */
+namespace Elminson\CodeWars;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class testCodeWars extends TestCase
+{
+
+    /**
+     *
+     */
+    function testFirstTestCase()
+    {
+        $codewars = new CodeWars();
+        $this->assertEquals("index", $codewars->index());
+    }
+
+}
